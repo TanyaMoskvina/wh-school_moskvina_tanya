a0c0cb9498147cae9cc4f020c32a13f8c77de3f1
TanyaMoskvina <moskv1na.ta1@gmail.com>
2023-09-25 16:56:41 +0300

    add schema shop and tables

---
+++ shop/tables/client.sql
@@ -0,0 +1,10 @@
+CREATE TABLE client
+(
+    client_id   integer     NOT NULL,
+    name        varchar(30) NOT NULL,
+    phone       varchar(11) NOT NULL,
+    dt          timestamptz NOT NULL,
+    employee_id integer     NOT NULL,
+    CONSTRAINT pk_client       PRIMARY KEY (client_id),
+    CONSTRAINT uq_client_phone UNIQUE (phone)
+);
---
+++ shop/tables/sales.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales
+(
+    sales_id    integer     NOT NULL,
+    dt          timestamptz NOT NULL,
+    client_id   integer     NOT NULL,
+    product_id  integer     NOT NULL,
+    amount      integer     NOT NULL,
+    employee_id integer     NOT NULL,
+    CONSTRAINT pk_sales PRIMARY KEY (sales_id)
+);
