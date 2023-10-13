e13ad84c38d52af86e31e481e69393b5d852027d
TanyaMoskvina <moskv1na.ta1@gmail.com>
2023-10-13 17:30:35 +0300

    table employees_changes

---
+++ history/tables/employees_changes.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS history.employees_changes
+(
+    log_id         BIGSERIAL   NOT NULL
+        CONSTRAINT pk_employees_changes PRIMARY KEY,
+    employee_id    BIGINT      NOT NULL,
+    phone          VARCHAR(11) NOT NULL,
+    name           VARCHAR(64) NOT NULL,
+    birth_date     DATE        NOT NULL,
+    rank_id        SMALLINT    NOT NULL,
+    is_deleted     BOOLEAN     NOT NULL,
+    ch_employee_id BIGINT      NOT NULL,
+    ch_dt          TIMESTAMPTZ NOT NULL
+);
