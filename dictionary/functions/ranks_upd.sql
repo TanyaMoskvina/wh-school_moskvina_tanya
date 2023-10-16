0ee3e8a765af2ce48650fa3dd82b047da1290fc4
TanyaMoskvina <moskv1na.ta1@gmail.com>
2023-10-16 11:27:50 +0300

    ranks_upd + readme

--- dictionary/functions/ranks_upd.sql
+++ dictionary/functions/ranks_upd.sql
@@ -14,10 +14,18 @@ BEGIN
            s.salary
     INTO _rank_id, _name, _salary
     FROM JSONB_TO_RECORD(_src) AS s (rank_id SMALLINT,
-                                    name    VARCHAR(32),
-                                    salary  NUMERIC(10, 2))
+                                     name    VARCHAR(32),
+                                     salary  NUMERIC(10, 2))
              LEFT JOIN dictionary.ranks r
                        ON r.rank_id = s.rank_id;
+
+    IF EXISTS(SELECT r.name FROM dictionary.ranks r WHERE r.name = _name)
+        AND _rank_id IS NULL
+    THEN
+        RETURN public.errmessage(_errcode := 'dictionary.ranks_upd.name_exists',
+                                 _msg := 'Такая должность уже существует',
+                                 _detail := CONCAT('name = ', _name));
+    END IF;
 
     INSERT INTO dictionary.ranks as r (rank_id,
                                        name,
