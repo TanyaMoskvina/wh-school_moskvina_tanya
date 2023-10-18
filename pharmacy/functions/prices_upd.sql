df3511cbb271aa06140c75d97ae31b48e2611c43
TanyaMoskvina <moskv1na.ta1@gmail.com>
2023-10-18 13:24:43 +0300

    edit prices_upd

--- pharmacy/functions/prices_upd.sql
+++ pharmacy/functions/prices_upd.sql
@@ -4,26 +4,20 @@ CREATE OR REPLACE FUNCTION pharmacy.prices_upd(_src JSONB, _ch_employee_id INT) 
 AS
 $$
 DECLARE
-    _nm_id BIGINT;
-    _price NUMERIC(10, 2);
     _dt    TIMESTAMPTZ := NOW() AT TIME ZONE 'Europe/Moscow';
 BEGIN
-
-    SELECT s.nm_id,
-           s.price
-    INTO _nm_id, _price
-    FROM JSONB_TO_RECORD(_src) AS s (nm_id BIGINT,
-                                     price NUMERIC(10, 2));
 
     WITH ins_cte AS (
         INSERT INTO pharmacy.prices AS p (nm_id,
                                           price,
                                           ch_employee_id,
                                           ch_dt)
-            SELECT _nm_id,
-                   _price,
-                   _ch_employee_id,
-                   _dt
+            SELECT s.nm_id,
+                   s.price,
+                   _ch_employee_id AS ch_employee_id,
+                   _dt             AS ch_dt
+            FROM JSONB_TO_RECORD(_src) AS s (nm_id BIGINT,
+                                             price NUMERIC(10, 2))
             ON CONFLICT (nm_id) DO UPDATE
                 SET price          = excluded.price,
                     ch_employee_id = excluded.ch_employee_id,
