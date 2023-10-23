78959ee37616bb18c6b525cc874c2fd04b47a0e6
TanyaMoskvina <moskv1na.ta1@gmail.com>
2023-10-23 14:01:51 +0300

    edit

--- pharmacy/functions/goods_on_place_upd.sql
+++ pharmacy/functions/goods_on_place_upd.sql
@@ -4,32 +4,28 @@ CREATE OR REPLACE FUNCTION pharmacy.goods_on_place_upd(_src JSONB) RETURNS JSONB
 AS
 $$
 DECLARE
-    _nm_id    BIGINT;
-    _place_id BIGINT;
-    _quantity SMALLINT;
-    _is_taken BOOLEAN;
 BEGIN
-    SELECT s.nm_id,
-           s.place_id,
-           s.quantity,
-           s.is_taken
-    INTO _nm_id, _place_id, _quantity, _is_taken
-    FROM JSONB_TO_RECORD(_src) AS s (nm_id    BIGINT,
-                                     place_id BIGINT,
-                                     quantity SMALLINT,
-                                     is_taken BOOLEAN);
-
-    IF _is_taken
-    THEN
-        _quantity = - _quantity;
-    END IF;
+    WITH cte AS (
+        SELECT s.nm_id,
+               s.place_id,
+               s.quantity,
+               s.is_taken
+        FROM JSONB_TO_RECORD(_src) AS s (nm_id    BIGINT,
+                                         place_id BIGINT,
+                                         quantity SMALLINT,
+                                         is_taken BOOLEAN)
+    )
 
     INSERT INTO pharmacy.goods_on_place AS gp (nm_id,
                                                place_id,
                                                quantity)
-    SELECT _nm_id,
-           _place_id,
-           _quantity
+    SELECT c.nm_id,
+           c.place_id,
+           (SELECT CASE
+                       WHEN c.is_taken THEN -c.quantity
+                       ELSE c.quantity
+                       END)
+    FROM cte c
     ON CONFLICT (nm_id, place_id) DO UPDATE
         SET quantity = gp.quantity + excluded.quantity;
 
--- pharmacy/functions/sales_ins.sql
+++ pharmacy/functions/sales_ins.sql
@@ -32,7 +32,7 @@ BEGIN
                                          responsible_employee_id BIGINT)
         RETURNING s.*)
 
-    , ins_his AS (
+/*    , ins_his AS (
         INSERT INTO history.sales_changes AS sc (sale_id,
                                                  client_id,
                                                  is_delivery,
@@ -51,13 +51,15 @@ BEGIN
                ic.responsible_employee_id,
                ic.ch_employee_id,
                ic.ch_dt
-        FROM ins_cte ic)
+        FROM ins_cte ic)*/
 
     , goods AS (
-        SELECT g.nm_id,
+        SELECT ic.sale_id,
+               g.nm_id,
                g.quantity
         FROM JSONB_TO_RECORDSET(_goods) AS g (nm_id    BIGINT,
-                                              quantity SMALLINT))
+                                              quantity SMALLINT),
+            ins_cte ic)
 
     , ins_sale_items AS (
         INSERT INTO pharmacy.sale_items AS si (sale_id,
@@ -75,23 +77,44 @@ BEGIN
                ), 2) AS total_price
         FROM ins_cte ic
                  LEFT JOIN humanresource.clients_card cc ON ic.client_id = cc.client_id
-                 LEFT JOIN dictionary.card_levels cl ON cc.level_id = cl.level_id,
-            goods g
+                 LEFT JOIN dictionary.card_levels cl ON cc.level_id = cl.level_id
+                 LEFT JOIN goods g ON ic.sale_id = g.sale_id
                  LEFT JOIN pharmacy.prices p ON g.nm_id = p.nm_id
         RETURNING si.total_price)
 
-    UPDATE humanresource.clients_card cc
-    SET amount_spent = cc.amount_spent + (SELECT SUM(si.total_price) FROM ins_sale_items si),
-        level_id = (SELECT CASE
-                               WHEN cc.level_id <= 4 AND
-                                   (cc.amount_spent + (SELECT SUM(si.total_price) FROM ins_sale_items si)) >= cl.amount_spent
-                                   THEN cc.level_id + 1
-                               ELSE cc.level_id
-                               END
-                    FROM dictionary.card_levels cl
-                    WHERE cl.level_id = cc.level_id)
-    FROM ins_cte ic
-    WHERE cc.client_id = ic.client_id;
+    , upd_cte AS (
+        UPDATE humanresource.clients_card cc
+        SET amount_spent = cc.amount_spent + (SELECT SUM(si.total_price) FROM ins_sale_items si),
+            level_id = (SELECT CASE
+                                   WHEN cc.level_id <= (SELECT MAX(cl.level_id) FROM dictionary.card_levels cl) AND
+                                       (cc.amount_spent + (SELECT SUM(si.total_price) FROM ins_sale_items si)) >= cl.amount_spent
+                                       THEN cc.level_id + 1
+                                   ELSE cc.level_id
+                                   END
+                        FROM dictionary.card_levels cl
+                        WHERE cl.level_id = cc.level_id)
+        FROM ins_cte ic
+        WHERE cc.client_id = ic.client_id)
+
+    INSERT INTO history.sales_changes AS sc (sale_id,
+                                             client_id,
+                                             is_delivery,
+                                             delivery_info,
+                                             status,
+                                             dt,
+                                             responsible_employee_id,
+                                             ch_employee_id,
+                                             ch_dt)
+    SELECT ic.sale_id,
+           ic.client_id,
+           ic.is_delivery,
+           ic.delivery_info,
+           ic.status,
+           ic.dt,
+           ic.responsible_employee_id,
+           ic.ch_employee_id,
+           ic.ch_dt
+    FROM ins_cte ic;
 
     RETURN JSONB_BUILD_OBJECT('data', NULL);
 END
